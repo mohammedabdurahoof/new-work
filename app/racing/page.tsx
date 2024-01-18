a736a1ef2da3f3fab8688966e5014f857bd519e2
mohammedabdurahoof <mohammedabdurahoof527@gmail.com>
2024-01-18 23:20:02 +0530

    racing page set

--- app/racing/page.tsx
+++ app/racing/page.tsx
@@ -1,14 +1,12 @@
 import { Chat, Footer, Navbar, SportsHeader, TopButton } from '@/components'
 import React from 'react'
+// import '../../styles/racing.css'
 
 function page() {
     return (
-        <div id="root" className="sports-layout g7">
-            <SportsHeader />
-            
-            <Footer />
-            <Chat />
-            <TopButton />
+        <div style={{height:'100vh'}}>
+
+            <iframe src="https://bc-games.appservices.app/?session=d0JrQ3R2eW9OZ2U5cnNiTHNlc0xkVWFZNHVsaE1sNmxoUFdldGtyaitnS1U3RWh5a0g2VVBucG05d0xFVVFzMnlPTHV2L21jTlI3QTZwUGpkUTUzYkVtQncvZW1sZENFKzk5RWhLVFV2VUtNOGJTQVViNTlEeElYYVpqN1Q0cDg=&amp;currency=USD-BTC-USDFIAT&amp;language=en" width="100%" height="100%" frameBorder="0"></iframe>
         </div>
     )
 }
